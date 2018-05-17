42251f0d8c61e5b1fafca1a28e9ed1c560db2006
GLYASAI <chaoschunqfeijg@163.com>
2018-05-17 22:38:33 +0800

    第8章 虚拟机字节码执行引擎

---
+++ src/main/java/org/abewang/chapter8/Calc.java
@@ -0,0 +1,16 @@
+package org.abewang.chapter8;
+
+/**
+ * javap -verbose Calc, 输出附加信息
+ *
+ * @Author Abe
+ * @Date 2018/5/16.
+ */
+public class Calc {
+    public int calc() {
+        int a = 100;
+        int b = 200;
+        int c = 300;
+        return (a + b) * c;
+    }
+}
---
+++ src/main/java/org/abewang/chapter8/InvokeDynamicTest.java
@@ -0,0 +1,46 @@
+package org.abewang.chapter8;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodType;
+
+import static java.lang.invoke.MethodHandles.lookup;
+
+/**
+ * 访问祖类的方法
+ *
+ * @Author Abe
+ * @Date 2018/5/17.
+ */
+public class InvokeDynamicTest {
+    class GrandFather {
+        void thinking() {
+            System.out.println("i am grandfather");
+        }
+    }
+
+    class Father extends GrandFather {
+        @Override
+        void thinking() {
+            System.out.println("i am father");
+        }
+    }
+
+    class Son extends Father {
+        @Override
+        void thinking() {
+            MethodType mt = MethodType.methodType(void.class);
+            try {
+                MethodHandle mh = lookup().findSpecial(GrandFather.class, "thinking", mt, Son.class);
+                mh.invoke(this);
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        InvokeDynamicTest test = new InvokeDynamicTest();
+        Son son = test.new Son();
+        son.thinking();
+    }
+}
